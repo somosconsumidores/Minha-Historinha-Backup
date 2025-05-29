ef32abff1e2d3df4037f58b734c3e5c9aca147a9
somosconsumidores <sandro.alves.leao@gmail.com>
2025-05-29 18:21:23 -0300

    FIX: Add QueryClientProvider to enable React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Create a client instance
+const queryClient = new QueryClient();
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+} else {
+  console.error("Failed to find the root element. Ensure your HTML has an element with ID 'root'.");
+}
